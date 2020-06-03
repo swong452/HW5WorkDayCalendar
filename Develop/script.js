874d3cf510c8ed736a4781a51b35f66720e7febc
samson wong <steelerwong2005@hotmail.com>
2020-06-03 19:49:15 -0400

    should be all good

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
+// Display current weekday, month, and date
 $("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMM Do'));
-//var Ninehour = document.querySelector("#\\39hr");
 
 compareTime();
 UserEvents();
@@ -9,20 +9,18 @@ UserEvents();
 // May be use this later: if(momentTime.isBefore(laterMomentTime)){
 function compareTime() {
     var currentTime = parseInt(moment().format('HH'));
-    //console.log("Current Hr is: ", currentTime, typeof(currentTime));
-    for (time=9;time < 21; time++) {
+    
+    //Start from 8AM, til 8PM
+    for (time=8;time < 21; time++) {
         var timeblock = parseInt($("#" +time+"hr").attr("data-index"));
-        //console.log("Time block is: ", timeblock, typeof(timeblock));
 
         if (timeblock < currentTime) {
             $("#"+time+"hr").css("background","lightgrey");
         } 
         else if (timeblock == currentTime) {
-        //else if (timeblock == currentTime) {
             $("#"+time+"hr").css("background","salmon");
         }
         else if (timeblock > currentTime) {
-            //console.log("Time block hr is Larger than current time", timeblock );
             $("#"+time+"hr").css("background","aqua");
         }
     } // End For 
@@ -32,63 +30,39 @@ function compareTime() {
 
 // function UserEvents display local stored event when screen is displayed
 function UserEvents() {
-    for (i = 9; i < 21; i++) {
+
+    for (i = 8; i < 21; i++) {
         var displayevent="";
         displayevent = localStorage.getItem("eventTime" + i);
         //console.log("Test local storage report successful: ", localStorage.getItem("eventTime" + i), "at time hr: ", i);
         console.log("Test local storage report successful: ", displayevent, "at time hr: ", i);
-        $("#"+i+"hr").value = displayevent;
-        $("#"+i+"hr").textContent = displayevent;
-        $("#"+i+"hr").textContent = "WHY NOT";
-        $("#"+i+"hr").innerHTML = displayevent;
-        console.log("The object inside Userevents are: ", $("#"+i+"hr").value);
-        console.log("The object inside Userevents are: ", $("#"+i+"hr").textContent);
-        console.log("The object inside Userevents are: ", $("#"+i+"hr").innerHTML);
-        
+        document.getElementById(i+"hr").value = displayevent;
 
-        console.log("The object inside Userevents are: ", $("#"+i+"hr"));
-        $("#10hr").text("Static Content");
-       // console.log("Static objec content", $("#10hr").text);
-    }
-}
+        //$("#"+i+"hr").value = displayevent;
+        //$("#"+i+"hr").textContent = displayevent;
+        //$("#"+i+"hr").textContent = "WHY NOT";
+        //console.log("The object inside Userevents are: ", $("#"+i+"hr").value);
+        //console.log("The object inside Userevents are: ", $("#"+i+"hr").textContent);
+        //console.log("The object inside Userevents are: ", $("#"+i+"hr").innerHTML);
+    } // End For
+} // End Function UserEvents
 
 
-// Listen to any user input on time block
-$(".hr").on("click", eventupdate);
-function eventupdate(event) {
-    var userevent = $(this);
-    console.log("Clicked index is", $(this).attr("data-index"));
-}
 
-// Listen to save button clicked.
+// Event Listener: Save user Input.
 // once clicked, check previous sibling value (text content)
-// Function saveEvent will save the userinput event into local storage
-// each hour has its own local storage , format "eventTime<hr>"
+// On the previous sibling (the input text element), extract the data-index value.
+// data-index will be used later, to uniquely append to each hr local storage var "eventTime" + dataIndex.
+// Next, save user typed event, from the input text element "val()", into local var "toDoList" 
+// save toDolist, into a local storage of that hour 
+// each hour has its own local storage , format "eventTime <hr>", where hr = previous extracted data-index.
 $(".saveBtn").on("click", saveEvent);
 function saveEvent(event) {
     var prevchild = $(this).prev();
     var eventHr = prevchild.attr("data-index");
     var toDolist = "";
-
-    console.log("Previous Sibling is:", prevchild);
-    console.log("Previous Sibling is:", prevchild.attr("data-index"));
-    console.log("Previous Sibling text is:", prevchild.val());
     toDolist = prevchild.val();
     localStorage.setItem("eventTime"+eventHr, toDolist )
 } 
-
-
-
-
-
-
-
-//++++++++++++++ EXTRA +++++++++++++++++++++++++++
-// Command to set the color of the input box background
-//$("#10").css("background","red");
-
-// Get time block hour value
-//console.log("Time block 9AM , data-index value is:", $("#9").attr("data-index"), typeof($("#9").attr("data-index")));
-//var blockhr = $("#9AM").attr("data-index");
 
 
